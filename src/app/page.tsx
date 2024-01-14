a6db9f2cfea1c6bf315cf6bbe50e0874c099eb4e
Brandon Marrero <brandon.marrero@myyahoo.com>
2024-01-14 12:26:00 -0500

    Add reusable info card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,35 @@
+import InfoCard from '@/components/InfoCard'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1>CM 360</h1>
-        <h2>Fine-Tuning Your Operations</h2>
+    <main className="flex min-h-screen flex-col items-center justify-between p-20">
+      <div className='container grid grid-cols-3 gap-2'>
+        <div className='grid col-span-2 grid-cols-4'>
+          <InfoCard 
+            title='High Priority'
+            content={24}
+          />
+          <InfoCard 
+            title='Pending Assignment'
+            content={13}
+          />
+          <InfoCard 
+            title='Open Issues'
+            content={24}
+          />
+          <InfoCard 
+            title='Total Monthly Spend'
+            content='$27,468.87'
+          />
+        </div>
+
+        <div className='flex w-full justify-end'>
+          <InfoCard
+            title='Placeholder Title'
+            content='37'
+          />
+        </div>
       </div>
     </main>
   )
