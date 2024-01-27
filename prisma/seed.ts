b3089a54b2e24891ca11bfa063d140fe66aaeb5a
Brandon Marrero <brandon.marrero@myyahoo.com>
2024-01-27 15:59:52 -0500

    add client table and relations and constraints

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,15 +1,37 @@
 import prisma from "../lib/prisma"
 
 async function seed() {
+    const client = await prisma.client.upsert({
+        where: { client_id: 1 },
+        update: {},
+        create: {
+            name: 'Test Client',
+            primary_email: 'test@test.com',
+            primary_phone: '7576988839'
+        }
+    })
+
+    const user = await prisma.user.upsert({
+        where: { user_id: 1 },
+        update: {},
+        create: {
+            first_name: 'Test',
+            last_name: 'User',
+            client_id: 1,
+            email: 'test-user@test.com'
+        }
+    })
+
     const asset = await prisma.asset.upsert({
-        where: { identifier: 'A7392XKJ' },
+        where: { asset_id: 1, client_id: 1, identifier: 'A7392XKJ' },
         update: {},
         create: {
             name: 'Forklift',
-            identifier: 'A7392XKJ'
+            identifier: 'A7392XKJ',
+            client_id: 1
         }
     })
-    console.log(asset)
+    console.log(client, user, asset)
 }
 
 seed()
