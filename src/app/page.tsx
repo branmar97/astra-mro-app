f68dd0e704e359f5b495f64d76c73511141aff8c
Brandon Marrero <brandon.marrero@myyahoo.com>
2024-01-28 08:37:26 -0500

    add mui provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import prisma from "../../lib/prisma/prismaClient"
+import TextField from '@mui/material/TextField';
 
 export default async function Home() {
   const asset = await prisma.asset.findUnique({
@@ -9,6 +10,29 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-20">
+      <div className='flex flex-col space-y-3 w-96'>
+        <TextField
+          label='Email'
+          placeholder='Please enter a valid email.'
+          variant='outlined'
+          required
+          type='email'
+        />
+
+        <TextField
+          label='Password'
+          variant='outlined'
+          type='password'
+          required
+        />
+
+        <TextField
+          label='Confirm Password'
+          variant='outlined'
+          type='password'
+          required
+        />
+      </div>
       {asset?.name} - {asset?.identifier}
     </main>
   )
